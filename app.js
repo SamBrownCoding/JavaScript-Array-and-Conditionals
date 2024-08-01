85e90fa64a51a2db1eed450df2cca9429b5ef5e9
SamBrownCoding <166549412+SamBrownCoding@users.noreply.github.com>
2024-08-01 15:51:51 -0500

    Updated
    More lesson to learn with.

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ console.log("4. " + favMovies[3]);
 //TODO Define an array of numbers
 //TODO Calculate sum of the numbers
 //Output the result
-let myNums = [50,100];
+let myNums = [50, 100];
 
 // let sum = myNums[0] + myNums[1];
 // console.log(sum);
@@ -134,7 +134,7 @@ console.log(boo);
 /*
 "==" - "is equal to"
 */
-let bar = 4 == 5-4;
+let bar = 4 == 5 - 4;
 console.log(bar);
 
 let boo2 = "4";
@@ -183,23 +183,37 @@ console.log("NOT: " + (!false));
 
 // if Else Statements
 
-if (boo3 == 5){
+if (boo3 == 5) {
     console.log("HEY, I'm THE NUMBER 4!")
-} else{ 
+} else {
     console.log("HEY YOU'RE NOT NUMBER 4!")
 }
 
+/*
+THESE ARE FALSY VALUES
+================
+- false
+- 0
+- -0
+- 0n
+- ""
+- '' (empty strings)
+- null
+- undefined
+- NaN
+=================
+*/
 
+//ELSE if Structure
 let amHungry = true;
 
-if(amHungry == true) {
+if (amHungry == true) {
     console.log("Let's eat! I want Chicken nuggies!");
 } else {
     console.log("Let's keep coding! Yay!");
 }
 
 
-//ELSE if Structure
 
 let howHungry = 0;
 
@@ -207,9 +221,9 @@ if (amHungry === true && howHungry >= 75) {
     console.log("I'm so hungry I can eat a horse.")
 } else if (amHungry === true && howHungry >= 25) {
     console.log("I'm hungry enough to eat cheez-its.")
-} else if (amHungry === false && howHungry >=25) {
+} else if (amHungry === false && howHungry >= 25) {
     console.log("I'm depressed.")
-}else {
+} else {
     console.log("I'm not that hungry!")
 }
 
@@ -218,10 +232,168 @@ if (amHungry === true && howHungry >= 75) {
 switch (amHungry == true) {
     case howHungry >= 75:
         console.log("I'm so hungry I can eat a horse.")
-    break;
-    case howHungry >=25:
+        break;
+    case howHungry >= 25:
         console.log("I'm hungry enough to eat cheez-its.")
-    break;
-default: 
-    console.log("I just wanna eat to eat.");
+        break;
+    default:
+        console.log("I just wanna eat to eat.");
 }
+
+
+
+//!CONDITIONS PROBLEMS
+
+//! Favorite Fruit Checker
+//TODO Define an array of favorite fruits
+//TODO Define a variable for the fruit to check
+//* Check if the fruit is in the array and output the result
+
+let favFruits = ["strawberry", "date", "banana", "watermelon"];
+
+let fruitToCheck = "strawberry";
+
+//Play with includes part
+if (favFruits.includes(fruitToCheck)) {
+    console.log("I love those fruit!");
+} else {
+    console.log("It's not my favorite fruit!");
+}
+
+if (fruitToCheck == favFruits[0] || fruitToCheck == favFruits[1] || fruitToCheck == favFruits[2] || fruitToCheck == favFruits[3]) {
+    console.log("It is my favorite!!");
+} else {
+    console.log("It ain't my favorite!");
+}
+
+//if else if ELSE statement
+if (fruitToCheck == favFruits[0]) {
+    console.log("Strawberry is good stuff!");
+} else if (fruitToCheck == favFruits[1]) {
+    console.log("Date is good replacement for sugar to mix with other!");
+} else if (fruitToCheck == favFruits[2]) {
+    console.log("Eat a banana once or twice per day!");
+} else if (fruitToCheck == favFruits[3]) {
+    console.log("Watermelon is great stuff for the summer time!");
+} else {
+    console.log("Sorry, Whatever it is! I don't want it!");
+}
+
+
+
+
+//!Grade Categorizer
+//TODO Define a variable for the grade
+let grade = 50;
+//TODO Categorize the grade
+let checkgrade;
+let message;
+
+if (grade >= 90) {
+    checkgrade = "A";
+} else if (grade >= 80) {
+    checkgrade = "B";
+} else if (grade >= 70) {
+    checkgrade = "C";
+} else if (grade >= 60) {
+    checkgrade = "D";
+} else if (grade <= 59) {
+    checkgrade = "F";
+    message = "He gotta pump up a rookie numbers!";
+} else {
+    console.log("Invalid number");
+}
+//* output the result
+
+console.log(`Student grade is ${grade} which it is a ${checkgrade}!! ${message}`);
+
+
+
+//! Day of the Week Checker
+//TODO Define an array of days of the week
+let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//TODO Define a variable for the day to check
+let checkDay = "Monday";
+//TODO Check if the day is a weekday or weekend
+let checkWeek;
+
+if (checkDay == week[0] || checkDay == week[6]) {
+    checkWeek = "weekend";
+} else {
+    checkWeek = "weekday";
+}
+//* output the result
+console.log(`${checkDay} is a ${checkWeek}!`);
+
+// SWITCH STATEMENT
+switch (checkDay) {
+    case week[0]:
+        console.log("It's the weekend")
+        break;
+    case week[1]:
+        console.log("It's the weekday");
+        break;
+    case week[2]:
+        console.log("It's the weekday");
+        break;
+    case week[3]:
+        console.log("It's the weekday");
+        break;
+    case week[4]:
+        console.log("It's the weekday");
+        break;
+    case week[5]:
+        console.log("It's the weekday");
+        break;
+    case week[6]:
+        console.log("It's the weekend")
+        break;
+    default:
+        console.log("Unknown");
+}
+
+
+//! Temperature Checker
+//TODO Define a variable for the temperature
+let curTemp = 87;
+//TODO Categorize the temperature and
+let result;
+
+if (curTemp >= 90) {
+    result = "It's hot as hades!";
+} else if (curTemp >= 80) {
+    result = "It's still hot";
+} else if (curTemp >= 70) {
+    result = "It's is nice cool weather";
+} else if (curTemp >= 60) {
+    result = "It's is very cool.";
+} else if (curTemp <= 59) {
+    result = "Bzzz bzzz It's...very...cold!";
+} else {
+    result = "Don't know what planet you are on!";
+}
+//* output the result
+
+console.log(`Weather is ${curTemp}\u00b0 and ${result}`);
+
+
+//! Number Sign Checker
+//TODO Define a variable for the number
+let signNum = -69;
+//TODO Check the sign of the number
+let checkNum;
+
+if (signNum === 69) {
+    checkNum = "Nice, but seriously it's positive!";
+} else if (signNum === -69) {
+    checkNum = "Nice, but you have a problem because it is negative!!"
+} else if (signNum >= 0) {
+    checkNum = "It's positive.";
+} else if (signNum < 0) {
+    checkNum = "It's negative!!";
+} else {
+    checkNum = "Unreadable"
+}
+//* output the result
+console.log(`Your sign is ${signNum} and result: ${checkNum}`);
+
